155b314d4de6ae217738cc9f813400a8a837f5c5
iwppa <564307515@qq.com>
2019-07-03 20:55:01 +0800

    分页

--- src/main/java/com/cskaoyan/mall/MallApplication.java
+++ src/main/java/com/cskaoyan/mall/MallApplication.java
@@ -1,9 +1,13 @@
 package com.cskaoyan.mall;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@MapperScan("com.cskaoyan.mall.mapper")
+@ServletComponentScan
 public class MallApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/cskaoyan/mall/service/impl/GoodsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.cskaoyan.mall.service.impl;
+
+import com.cskaoyan.mall.bean.Goods;
+import com.cskaoyan.mall.mapper.GoodsMapper;
+import com.cskaoyan.mall.service.GoodsService;
+import com.cskaoyan.mall.vo.DataVo;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GoodsServiceImpl implements GoodsService {
+
+
+    @Autowired
+    GoodsMapper goodsMapper;
+
+    @Override
+    public DataVo<Goods> findGoodsList(int page, int limit, String sort, String order) {
+        PageHelper.startPage(page,limit);
+        List<Goods> items =  goodsMapper.findGoodsList(sort,order);
+        PageInfo<Goods> pageInfo = new PageInfo<Goods>(items);
+        DataVo<Goods> goodsDataVo = new DataVo<Goods>();
+        goodsDataVo.setTotal((int)pageInfo.getTotal());
+        goodsDataVo.setItems(pageInfo.getList());
+        return goodsDataVo;
+    }
+}
