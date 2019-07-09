5b88f9e6c76463dae6773d9bdafa11cff6bad562
Applewald <hexi.aoyu@163.com>
2019-07-09 09:39:27 +0800

    hxy's8thPush

---
+++ src/main/java/com/cskaoyan/mall/wx/controller/PromotionController.java
@@ -0,0 +1,12 @@
+package com.cskaoyan.mall.wx.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 河鲍鱼
+ * 建立于 2019/7/8 22:11
+ */
+@RestController
+public class PromotionController {
+
+}
